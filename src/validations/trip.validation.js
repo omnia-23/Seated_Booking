47851550d4e537e487f959d5c364133f6a57c562
omnia <omniagida@gmail.com>
2024-05-17 00:50:02 +0200

    update add trip

--- src/validations/trip.validation.js
+++ src/validations/trip.validation.js
@@ -18,7 +18,6 @@ export const addTripSchema = Joi.object({
   Trip_Start_Date: Joi.string().required(),
   Trip_End_Date: Joi.string().required(),
   Vehicle_ID: Joi.string().custom(isObjectId, "ObjectId validation").required(),
-  Seat_Price: Joi.number().required(),
   Trip_Notes: Joi.string().required(),
 });
 
@@ -28,6 +27,5 @@ export const updateTripSchema = Joi.object({
   Trip_Start_Date: Joi.string(),
   Trip_End_Date: Joi.string(),
   Vehicle_ID: Joi.string().custom(isObjectId, "ObjectId validation"),
-  Seat_Price: Joi.number(),
   Trip_Notes: Joi.string(),
 });
